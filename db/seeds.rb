98af611cf07bf5cccf2eb4c4db8e3e97d926d9f9
lengcb <649569822@qq.com>
2023-09-25 10:34:26 +0800

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+unless AdminUser.find_by(login_name: 'admin_leng')
+  AdminUser.create!login_name: 'admin_leng', password: 'admin_leng!', password_confirmation: 'admin_leng!!', phone: '15275200369', nickname: '冷传宝', grade: 1
+end
